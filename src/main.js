a65933b098b129285b6dc8bcd38a6fe01d108fa9
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-15 20:42:29 -0600

    adding props

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+
+const myObject = {
+  name: "World",
+  thing: "7 seasons and a movie",
+};
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+		myThing: "6 seasons and a movie",
+		myObject: myObject
+  },
 });
 
 export default app;
